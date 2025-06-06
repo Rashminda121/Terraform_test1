955bb71cd957637f84fc0ed2e8929271e7b4f5be
Jayamuni Rashminda <rashminda121@gmail.com>
2025-06-07 01:06:32 +0530

    feat: variables.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,6 @@ resource "tls_private_key" "rsa_4096" {
   rsa_bits  = 4096
 }
 
-variable "key_name" {
-  default = "terraform_pem"
-}
-
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = tls_private_key.rsa_4096.public_key_openssh
